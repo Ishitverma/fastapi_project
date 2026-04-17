a72b249092922200fec6692fab519f35afa6ea84
Ishit Verma <ishit.verma@dataoids.com>
2026-04-17 19:15:41 +0530

    Added auth system and updated models

--- schemas.py
+++ schemas.py
@@ -9,7 +9,21 @@ class UserBase(BaseModel):
 
 
 class UserCreate(UserBase):
-    pass
+    password: str = Field(min_length=8)
+
+
+class UserPublic(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+    username: str
+    image_file: str | None
+    image_path: str
+
+
+class UserPrivate(UserPublic):
+    email: EmailStr
+
 
 class UserUpdate(BaseModel):
     username: str | None = Field(default=None, min_length=1, max_length=50)
@@ -17,31 +31,29 @@ class UserUpdate(BaseModel):
     image_file: str | None = Field(default=None, min_length=1, max_length=200)
 
 
-class UserResponse(UserBase):
-    model_config = ConfigDict(from_attributes=True)
+class Token(BaseModel):
+    access_token: str
+    token_type: str
 
-    id: int
-    image_file: str | None
-    image_path: str | None
 
-class PostBase2(BaseModel):
+class PostBase(BaseModel):
     title: str = Field(min_length=1, max_length=100)
     content: str = Field(min_length=1)
 
 
-class PostCreate2(PostBase2):
+class PostCreate(PostBase):
     user_id: int  # TEMPORARY
-    
+
+
 class PostUpdate(BaseModel):
     title: str | None = Field(default=None, min_length=1, max_length=100)
     content: str | None = Field(default=None, min_length=1)
 
 
-
-class PostResponse2(PostBase2):
+class PostResponse(PostBase):
     model_config = ConfigDict(from_attributes=True)
 
     id: int
     user_id: int
     date_posted: datetime
-    author: UserResponse
+    author: UserPublic
